00e6405a8412af0553e00fc835e45229969ce0d2
Violetabaltazar <114529977+Violetabaltazar@users.noreply.github.com>
2023-05-08 11:55:23 -0600

    Delete Semana 6 directory

--- Semana 6/PruebasIntegracion/SpringBootTest/src/test/resources/init.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO paises (id, nombre, poblacion) VALUES
-    (1, 'Mexico', 130497248),
-    (2, 'Espana', 49067981);
--- Semana 6/PruebasUnitarias/JUnit/src/main/java/paquete4/Transaction.java
+++
@@ -1,5 +0,0 @@
-package paquete4;
-
-public interface Transaction {
-    Double getAmount();
-}
